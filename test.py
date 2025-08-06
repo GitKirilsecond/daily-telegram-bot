8bf0079bb0ebf5506fe54f0ad50947766c84796c
Kiril <kiril.strykalenko@icloud.com>
2025-08-06 18:19:47 +0300

    change to 19:00

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-import telebot
-import datetime
+import time
+import requests
+from datetime import datetime
 
 BOT_TOKEN = '8259339256:AAFLtjDITQTo1-W2yigShG7QSMW1vzICMgI'
 CHANNEL_USERNAME = '@testtgforme'
 
-now = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=3)  # Київський час
-if now.hour == 12 and 30 <= now.minute <= 59:
-    text = f"[{now.strftime('%H:%M:%S')}] Повідомлення через Render кожні 5 хвилин (12:30–12:59)"
+def send_message(text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {'chat_id': CHANNEL_USERNAME, 'text': text}
+    payload = {
+        'chat_id': CHANNEL_USERNAME,
+        'text': text
+    }
     requests.post(url, data=payload)
+
+# Тест: 5 повідомлень з інтервалом 10 хв
+for i in range(5):
+    now = datetime.now().strftime("%H:%M:%S")
+    send_message(f"[{now}] Тестове повідомлення #{i+1}")
+    if i < 4:
+        time.sleep(600)  # 10 хвилин = 600 секунд
